a34f4cb0769028d03e698fb2450adef959ab010f
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@dsn127m1246726.cfp127aluno.local>
2026-02-24 15:39:34 -0300

    validação do vazio finalizada, e também a validação do caixa vazia

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/YhZcvi3u)
+
+1 Apagar o texto da caixinha quando for adicionado algo
+2 validar o vazio
+3 codigo, produto e quantidade
--- main.js
+++ main.js
@@ -7,8 +7,32 @@ function adicionarProduto(){
     const span = document.createElement('span')
     span.textContent = produto.value
     
+    let nomeProduto = validarDado(produto.value)
+
+
+       if(nomeProduto){
+        span.className = 'bg-blue-200 py-2 px-8'    
+        lista.appendChild(span)
+        DeixaCampoVazio()
+       }else{
+        return false
+       }
+
+       
+    }
+
+    function validarDado(produto){
+
+        let nomeProduto = produto
+        if(nomeProduto.value == ""){
+            return false
+        }else{
+            return nomeProduto
+        }
+
+    }
+
+    function DeixaCampoVazio(){
+        document.getElementById('produto').value = ''
+    }
     
-    
-    
-    span.className = 'bg-blue-200 py-2 px-8'    
-    lista.appendChild(span)}
